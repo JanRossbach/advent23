fe205addfb1d27785b3167f681e9bed821df98a7
JanRossbach <janrossbach3@gmail.com>
2023-12-03 17:43:08 +0000

    Gets Test data but real data is too low

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 pub struct Part {
     part_num: usize,
 }
@@ -42,12 +44,7 @@ fn parse_input(input: &str) -> Grid {
 }
 
 fn is_symbol(c: &char) -> bool {
-    match c {
-        '*' => true,
-        '$' => true,
-        '+' => true,
-        _ => false,
-    }
+    !c.is_digit(10) && *c != '.' && *c != ' '
 }
 
 /// Extract the digits that are before the symbol at (i, j).
@@ -93,56 +90,55 @@ fn extract_num(input: &Grid, i: usize, j: usize) -> u32 {
 /// Find a digit that is adjacent to the symbol at (i, j) and return the number that it is part of.
 /// The digit can be at the beginning, middle or end of the number.
 /// The digit can be in any direction (up, down, left, right) and also diagonal.
-fn find_part_num(input: &Grid, i: usize, j: usize) -> u32 {
+fn find_part_nums(input: &Grid, i: usize, j: usize) -> Vec<u32> {
+    let mut nums: HashSet<u32> = HashSet::new();
     let upper_left = input.get(i - 1, j - 1);
-    let mut pn = 0;
     if upper_left.is_digit(10) {
-        pn += extract_num(input, i - 1, j - 1);
+        nums.insert(extract_num(input, i - 1, j - 1));
     }
     let upper = input.get(i - 1, j);
     if upper.is_digit(10) {
-        pn += extract_num(input, i - 1, j);
+        nums.insert(extract_num(input, i - 1, j));
     }
     let upper_right = input.get(i - 1, j + 1);
     if upper_right.is_digit(10) {
-        pn += extract_num(input, i - 1, j + 1);
+        nums.insert(extract_num(input, i - 1, j + 1));
     }
     let left = input.get(i, j - 1);
     if left.is_digit(10) {
-        pn += extract_num(input, i, j - 1);
+        nums.insert(extract_num(input, i, j - 1));
     }
     let right = input.get(i, j + 1);
     if right.is_digit(10) {
-        pn += extract_num(input, i, j + 1);
+        nums.insert(extract_num(input, i, j + 1));
     }
     let lower_left = input.get(i + 1, j - 1);
     if lower_left.is_digit(10) {
-        pn += extract_num(input, i + 1, j - 1);
+        nums.insert(extract_num(input, i + 1, j - 1));
     }
     let lower = input.get(i + 1, j);
     if lower.is_digit(10) {
-        pn += extract_num(input, i + 1, j);
+        nums.insert(extract_num(input, i + 1, j));
     }
-    pn
+    nums.into_iter().collect::<Vec<u32>>()
 }
 
 pub fn extract_parts(input: &Grid) -> Vec<u32> {
-    let mut parts: Vec<u32> = Vec::new();
+    let mut parts: HashSet<u32> = HashSet::new();
     for (i, row) in input.grid.iter().enumerate() {
         for (j, c) in row.iter().enumerate() {
             if is_symbol(c) {
-                parts.push(find_part_num(input, i, j));
+                let nums = find_part_nums(input, i, j);
+                parts.extend(nums);
             }
         }
     }
-    parts
+    parts.into_iter().collect::<Vec<u32>>()
 }
 
-pub fn solve(input: &str, star2: bool) -> u32 {
+pub fn solve(input: &str, _star2: bool) -> u32 {
     let input = parse_input(input);
-    println!("{:?}", input.grid);
     let parts: Vec<u32> = extract_parts(&input);
-    println!("{:?}", parts);
     parts.iter().sum()
 }
 
@@ -158,6 +154,8 @@ mod tests {
 
     #[test]
     fn test_test_input_star1() {
+        let real_input = std::fs::read_to_string("data/input3.txt").unwrap();
         assert_eq!(solve(TEST_INPUT, false), 4361);
+        assert_eq!(solve(&real_input, false), 0);
     }
 }
